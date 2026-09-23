db1f418ac30868a7ebd72792faffa105c6da14cd
711576321ha <711576321ha@gmail.com>
2026-09-23 21:56:46 +0300

    Update project.cpp

--- project.cpp
+++ project.cpp
@@ -2,17 +2,27 @@
 using namespace std;
 
 int main() {
-    double num1, num2, sum;
-    
+    double num1, num2;
+    char op;
+
     cout << "ادخل الرقم الاول: ";
     cin >> num1;
-    
+
     cout << "ادخل الرقم الثاني: ";
     cin >> num2;
-    
-    sum = num1 - num2;
-    
-    cout << "الناتج هو: " << sum << endl;
-    
+
+    cout << "اختر العملية (+ للجمع و - للطرح): ";
+    cin >> op;
+
+    if (op == '+') {
+        cout << "الناتج هو: " << num1 + num2 << endl;
+    } 
+    else if (op == '-') {
+        cout << "الناتج هو: " << num1 - num2 << endl;
+    } 
+    else {
+        cout << "عملية غير صحيحة!" << endl;
+    }
+
     return 0;
 }
